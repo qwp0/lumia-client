b3e549b3d5468acc65be8aaa3d8ff87ff26bb4d4
Soyoung Park <soyoung008@naver.com>
2025-06-27 20:36:58 +0900

    chore: TailwindCSS 설치 및 기본 설정

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig({ plugins: [react()] });
+export default defineConfig({ plugins: [react(), tailwindcss()] });
