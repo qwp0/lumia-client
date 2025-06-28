a0feb5affa376ee5ecf486a6b35781137bee6bc5
Soyoung Park <soyoung008@naver.com>
2025-06-28 23:03:32 +0900

    feat: Uploader 컴포넌트 클릭 시 파일 선택창 열리는 기능 추가 #1
    - Uploader 컴포넌트 내 숨겨진 input 요소에 ref 연결
    - 영역 클릭 시 파일 선택창 트리거되도록 이벤트 핸들러 구현
    - 현재는 파일 처리 없이 정적 동작만 구현

--- src/components/home/Uploader.jsx
+++ src/components/home/Uploader.jsx
@@ -1,11 +1,28 @@
+import { useRef } from "react";
+
 import { FileUploadIcon } from "@/assets";
 
 const Uploader = () => {
+  const fileInputRef = useRef(null);
+
+  const handleClick = () => {
+    fileInputRef.current.click();
+  };
+
   return (
-    <section className="flex h-[300px] w-[800px] flex-col items-center justify-center gap-2 rounded-[10px] border-2 border-dashed border-gray-300 font-light">
+    <section
+      className="flex h-[300px] w-[800px] flex-col items-center justify-center gap-2 rounded-[10px] border-2 border-dashed border-gray-300 font-light"
+      onClick={handleClick}
+    >
       <FileUploadIcon />
+      <input
+        type="file"
+        accept=".pdf"
+        ref={fileInputRef}
+        className="hidden"
+      />
       <p>여기에 발표 자료를 드래그하거나 클릭해서 업로드하세요.</p>
-      <p className="text-gray-400">PDF, PNG, JPG</p>
+      <p className="text-gray-400">PDF</p>
     </section>
   );
 };
