e519d4a0fb713c3e556bbc856c147062721369d6
Soyoung Park <soyoung008@naver.com>
2025-06-29 17:56:48 +0900

    assets: 슬라이드 네비게이션을 위한 화살표 아이콘 추가 #2

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,5 @@
+import ArrowLeft from "@/assets/icons/arrow-left.svg?react";
+import ArrowRight from "@/assets/icons/arrow-right.svg?react";
 import CornerLeftBottomIcon from "@/assets/icons/corner-left-bottom.svg?react";
 import CornerLeftTopIcon from "@/assets/icons/corner-left-top.svg?react";
 import CornerRightBottomIcon from "@/assets/icons/corner-right-bottom.svg?react";
@@ -5,6 +7,8 @@ import CornerRightTopIcon from "@/assets/icons/corner-right-top.svg?react";
 import FileUploadIcon from "@/assets/icons/file-upload.svg?react";
 
 export {
+  ArrowLeft,
+  ArrowRight,
   CornerLeftBottomIcon,
   CornerLeftTopIcon,
   CornerRightBottomIcon,
