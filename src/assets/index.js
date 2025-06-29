8f9ee591c8039eb87bb8723270ed38842cad818a
Soyoung Park <soyoung008@naver.com>
2025-06-29 12:01:33 +0900

    assets: 파일 드래그 관련 아이콘 추가 #1

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,13 @@
+import CornerLeftBottomIcon from "@/assets/icons/corner-left-bottom.svg?react";
+import CornerLeftTopIcon from "@/assets/icons/corner-left-top.svg?react";
+import CornerRightBottomIcon from "@/assets/icons/corner-right-bottom.svg?react";
+import CornerRightTopIcon from "@/assets/icons/corner-right-top.svg?react";
 import FileUploadIcon from "@/assets/icons/file-upload.svg?react";
 
-export { FileUploadIcon };
+export {
+  CornerLeftBottomIcon,
+  CornerLeftTopIcon,
+  CornerRightBottomIcon,
+  CornerRightTopIcon,
+  FileUploadIcon,
+};
