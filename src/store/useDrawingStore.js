815ee5a7aa7fe4c5daf0d61ec186b6a06d7544eb
Soyoung Park <soyoung008@naver.com>
2025-07-01 19:06:17 +0900

    feat: persist 미들웨어를 이용한 페이지 드로잉 상태 로컬스토리지 저장 기능 구현 #6
    - Zustand 상태를 localStorage에 연동하여 페이지별 드로잉 내용 유지
    - partialize 옵션으로 필요한 상태만 저장

--- src/store/useDrawingStore.js
+++ src/store/useDrawingStore.js
@@ -1,48 +1,59 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 import { HIGHLIGHTER_COLORS, PEN_COLORS } from "@/constants/colors";
 import { ERASER_MODES } from "@/constants/tool";
 
-export const useDrawingStore = create((set, get) => ({
-  activeTool: null,
-  penColor: PEN_COLORS[0],
-  highlighterColor: HIGHLIGHTER_COLORS[0],
-  eraserMode: ERASER_MODES.PARTIAL,
-  isDeleteModalOpen: false,
-  canvasRef: null,
-  currentPage: 1,
-  pageDrawings: {},
+export const useDrawingStore = create(
+  persist(
+    (set, get) => ({
+      activeTool: null,
+      penColor: PEN_COLORS[0],
+      highlighterColor: HIGHLIGHTER_COLORS[0],
+      eraserMode: ERASER_MODES.PARTIAL,
+      isDeleteModalOpen: false,
+      canvasRef: null,
+      currentPage: 1,
+      pageDrawings: {},
 
-  setActiveTool: (activeTool) => set({ activeTool }),
-  setPenColor: (color) => set({ penColor: color }),
-  setHighlighterColor: (color) => set({ highlighterColor: color }),
-  setEraserMode: (mode) => set({ eraserMode: mode }),
-  setDeleteModalOpen: (isOpen) => set({ isDeleteModalOpen: isOpen }),
-  setCanvasRef: (ref) => set({ canvasRef: ref }),
-  setCurrentPage: (page) => set({ currentPage: page }),
-  setPageDrawings: (pageNumber, drawings) =>
-    set((state) => ({
-      pageDrawings: {
-        ...state.pageDrawings,
-        [pageNumber]: drawings,
+      setActiveTool: (activeTool) => set({ activeTool }),
+      setPenColor: (color) => set({ penColor: color }),
+      setHighlighterColor: (color) => set({ highlighterColor: color }),
+      setEraserMode: (mode) => set({ eraserMode: mode }),
+      setDeleteModalOpen: (isOpen) => set({ isDeleteModalOpen: isOpen }),
+      setCanvasRef: (ref) => set({ canvasRef: ref }),
+      setCurrentPage: (page) => set({ currentPage: page }),
+      setPageDrawings: (pageNumber, drawings) =>
+        set((state) => ({
+          pageDrawings: {
+            ...state.pageDrawings,
+            [pageNumber]: drawings,
+          },
+        })),
+
+      clearCurrentPageCanvas: () => {
+        const canvas = get().canvasRef;
+        const page = get().currentPage;
+
+        set((state) => ({
+          pageDrawings: {
+            ...state.pageDrawings,
+            [page]: { drawings: [] },
+          },
+        }));
+
+        if (canvas) {
+          const ctx = canvas.getContext("2d");
+
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+        }
       },
-    })),
-
-  clearCurrentPageCanvas: () => {
-    const canvas = get().canvasRef;
-    const page = get().currentPage;
-
-    set((state) => ({
-      pageDrawings: {
-        ...state.pageDrawings,
-        [page]: { drawings: [] },
-      },
-    }));
-
-    if (canvas) {
-      const ctx = canvas.getContext("2d");
-
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
-  },
-}));
+    }),
+    {
+      name: "drawing-storage",
+      partialize: (state) => ({
+        pageDrawings: state.pageDrawings,
+      }),
+    },
+  ),
+);
