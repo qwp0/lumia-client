a44a5f4725eb29647254d8c45deb8f769d6f1c88
Soyoung Park <soyoung008@naver.com>
2025-07-02 22:21:19 +0900

    feat: /presentation/:roomId, /audience/:roomId로 경로 수정 #9

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 
+import AudienceView from "@/pages/audienceView";
 import Home from "@/pages/Home";
 import Presentation from "@/pages/Presentation";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
-  { path: "/presentation", element: <Presentation /> },
+  { path: "/presentation/:roomId", element: <Presentation /> },
+  { path: "/audience/:roomId", element: <AudienceView /> },
 ]);
 
 export default router;
