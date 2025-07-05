cf329771ebbdd825eef4ccf5e01bbb9ab1698e0e
Soyoung Park <soyoung008@naver.com>
2025-07-06 03:14:00 +0900

    assets: 채팅에 필요한 아이콘 추가 #13

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,8 @@
 import ArrowLeft from "@/assets/icons/arrow-left.svg?react";
 import ArrowRight from "@/assets/icons/arrow-right.svg?react";
+import ChatIcon from "@/assets/icons/chat.svg?react";
 import CircleIcon from "@/assets/icons/circle.svg?react";
+import CloseIcon from "@/assets/icons/close.svg?react";
 import CopySuccessIcon from "@/assets/icons/copy-success.svg?react";
 import CornerLeftBottomIcon from "@/assets/icons/corner-left-bottom.svg?react";
 import CornerLeftTopIcon from "@/assets/icons/corner-left-top.svg?react";
@@ -8,6 +10,7 @@ import CornerRightBottomIcon from "@/assets/icons/corner-right-bottom.svg?react"
 import CornerRightTopIcon from "@/assets/icons/corner-right-top.svg?react";
 import CursorIcon from "@/assets/icons/cursor.svg?react";
 import DialogCheckIcon from "@/assets/icons/dialog-check.svg?react";
+import EmoticonIcon from "@/assets/icons/emoticon.svg?react";
 import EraserIcon from "@/assets/icons/eraser.svg?react";
 import ExitIcon from "@/assets/icons/exit.svg?react";
 import FileUploadIcon from "@/assets/icons/file-upload.svg?react";
@@ -16,13 +19,16 @@ import LineIcon from "@/assets/icons/line.svg?react";
 import LinkIcon from "@/assets/icons/link.svg?react";
 import PenIcon from "@/assets/icons/pen.svg?react";
 import PresentationIcon from "@/assets/icons/presentation.svg?react";
+import SendIcon from "@/assets/icons/send.svg?react";
 import SquareIcon from "@/assets/icons/square.svg?react";
 import TrashCanIcon from "@/assets/icons/trash-can.svg?react";
 
 export {
   ArrowLeft,
   ArrowRight,
+  ChatIcon,
   CircleIcon,
+  CloseIcon,
   CopySuccessIcon,
   CornerLeftBottomIcon,
   CornerLeftTopIcon,
@@ -30,6 +36,7 @@ export {
   CornerRightTopIcon,
   CursorIcon,
   DialogCheckIcon,
+  EmoticonIcon,
   EraserIcon,
   ExitIcon,
   FileUploadIcon,
@@ -38,6 +45,7 @@ export {
   LinkIcon,
   PenIcon,
   PresentationIcon,
+  SendIcon,
   SquareIcon,
   TrashCanIcon,
 };
