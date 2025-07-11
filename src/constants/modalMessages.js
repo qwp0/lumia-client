1d83a57677ac45b956ef6dd336e5854e53d26b80
Soyoung Park <soyoung008@naver.com>
2025-07-12 02:02:37 +0900

    style: 모달 메시지 구조 (MODAL_MESSAGES) 수정 #28

--- src/constants/modalMessages.js
+++ src/constants/modalMessages.js
@@ -1,14 +1,14 @@
 export const MODAL_MESSAGES = {
   endPresentation: {
     title: "발표를 종료하시겠습니까?",
-    description: "지금 종료하면 더 이상 발표를 이어갈 수 없습니다.",
-    confirmText: "종료",
-    cancelText: "취소",
+    description: "슬라이드와 주석을 PDF로 저장할 수 있어요.",
+    firstText: "종료",
+    secondText: "저장하고 종료",
   },
   deleteAll: {
     title: "모든 주석을 삭제하시겠습니까?",
     description: "삭제한 내용은 복구할 수 없습니다.",
-    confirmText: "삭제",
-    cancelText: "취소",
+    firstText: "취소",
+    secondText: "삭제",
   },
 };
