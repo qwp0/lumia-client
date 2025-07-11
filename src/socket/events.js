cede80f1272259199f2af2e030f022b44c332c2f
Soyoung Park <soyoung008@naver.com>
2025-07-12 03:21:06 +0900

    feat: 서버와의 데이터 구조 통일을 위해 message 필드를 text로 변경 #28

--- src/socket/events.js
+++ src/socket/events.js
@@ -4,13 +4,13 @@ export const joinRoom = (roomId, nickname) => {
   socket.emit("join_room", { roomId, nickname });
 };
 
-export const sendTextFeedback = ({ roomId, page, nickname, role, message }) => {
+export const sendTextFeedback = ({ roomId, page, nickname, role, text }) => {
   socket.emit("text-feedback", {
     roomId,
     page,
     nickname,
     role,
-    message,
+    text,
   });
 };
 
