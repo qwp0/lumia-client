812ef44bc6f03be00c086a47777f7c9b8c5783a7
Soyoung Park <soyoung008@naver.com>
2025-07-16 16:41:58 +0900

    feat: 청중 입장용 AudienceEntry 페이지 분리 및 라우터 연결 #33

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import Audience from "@/pages/Audience";
+import AudienceEntry from "@/pages/AudienceEntry";
 import Home from "@/pages/Home";
 import Presentation from "@/pages/Presentation";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/presentation/:roomId", element: <Presentation /> },
+  { path: "/enter/:roomId", element: <AudienceEntry /> },
   { path: "/audience/:roomId", element: <Audience /> },
 ]);
 
