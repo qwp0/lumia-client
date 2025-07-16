97cdc11c48fe10d7dc0fa6ee0a55ccb06a5f4bde
Soyoung Park <soyoung008@naver.com>
2025-07-16 23:33:32 +0900

    feat: isUnread 상태에 따라 빨간 점 렌더링되도록 구현 #36

--- src/components/common/chat/ChatToggleButton.jsx
+++ src/components/common/chat/ChatToggleButton.jsx
@@ -1,6 +1,9 @@
 import { ChatIcon, CloseIcon } from "@/assets";
+import { useChatStore } from "@/store/useChatStore";
 
 const ChatToggleButton = ({ onClick, isChatOpen }) => {
+  const isUnread = useChatStore((state) => state.isUnread);
+
   return (
     <button
       type="button"
@@ -8,11 +11,16 @@ const ChatToggleButton = ({ onClick, isChatOpen }) => {
       className="fixed right-6 bottom-6 z-30 rounded-full bg-white/90 p-3 shadow-md"
       title="피드백 보내기"
     >
-      {isChatOpen ? (
-        <CloseIcon className="h-6 w-6" />
-      ) : (
-        <ChatIcon className="pointer-events-none h-6 w-6 text-black" />
-      )}
+      <div className="relative">
+        {isChatOpen ? (
+          <CloseIcon className="h-6 w-6" />
+        ) : (
+          <ChatIcon className="pointer-events-none h-6 w-6 text-black" />
+        )}
+        {isUnread && (
+          <span className="absolute -top-1 -right-1 h-2 w-2 rounded-full bg-red-500" />
+        )}
+      </div>
     </button>
   );
 };
